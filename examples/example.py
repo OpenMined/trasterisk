3974ab24f067a0a9b61cfa82ef0170649e115edc
Madhava Jay <me@madhavajay.com>
2021-02-09 14:09:40 +1000

    Moved example code into external file

---
+++ examples/example.py
@@ -0,0 +1,14 @@
+class A:
+    @staticmethod
+    def foo_bad(forcenamed):
+        print(forcenamed)
+
+    @staticmethod
+    def foo_good(*, forcenamed):
+        print(forcenamed)
+
+    def bar_bad(self, forcenamed):
+        print(self, forcenamed)
+
+    def bar_good(self, *, forcenamed):
+        print(self, forcenamed)
--- tests/flake8_kwarger_test.py
+++ tests/flake8_kwarger_test.py
@@ -2,25 +2,11 @@
 # Author: github.com/madhavajay
 
 import ast
+import os
+from pathlib import Path
 from typing import Set
 
 from flake8_kwarger import Plugin
-
-example_1 = """class A:
-    @staticmethod
-    def foo_bad(forcenamed):
-        print(forcenamed)
-
-    @staticmethod
-    def foo_good(*, forcenamed):
-        print(forcenamed)
-
-    def bar_bad(self, forcenamed):
-        print(self, forcenamed)
-
-    def bar_good(self, *, forcenamed):
-        print(self, forcenamed)
-"""
 
 
 def _results(s: str) -> Set[str]:
@@ -33,9 +19,15 @@ def test_trivial_case():
     assert _results("") == set()
 
 
-def test_missing_kwargs_only():
-    ret = _results(example_1)
+def test_missing_kwargs():
+    examples_dir = Path(os.path.dirname(__file__)) / ".." / "examples"
+    example_file = examples_dir / "example.py"
+    with open(example_file) as f:
+        example = "".join(f.readlines())
+
+    ret = _results(example)
     msg = "FKO100 Non Keyword-Only Arguments not allowed. Try adding a '*'."
+
     expected = {
         f"3:5 {msg}",
         f"10:5 {msg}",
